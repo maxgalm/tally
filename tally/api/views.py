73122fd3a797b6523c92a674bf196625375bda65
LorisSchell <loris.schell@t-online.de>
2024-04-27 01:30:02 +0200

    added single endpoints for all views

--- tally/api/views.py
+++ tally/api/views.py
@@ -7,10 +7,25 @@ class PersonListCreate(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
+class PersonRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    lookup_field = "pk"
+
 class DrinkListCreate(generics.ListCreateAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
 
+class DrinkRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
+    lookup_field = "pk"
+
 class TallyListCreate(generics.ListCreateAPIView):
     queryset = Tally.objects.all()
     serializer_class = TallySerializer
+
+class TallyRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tally.objects.all()
+    serializer_class = TallySerializer
+    lookup_field = "pk"
