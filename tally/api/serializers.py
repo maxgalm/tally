cae301cc270d73aba54a5c2c7b8dbe56227cf4b3
LorisSchell <loris.schell@t-online.de>
2024-04-27 00:44:09 +0200

    added serializers to the project

---
+++ tally/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Person, Drink, Tally
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ["id", "first_name", "last_name", "date_joined"]
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ["id", "name", "description", "price"]
+
+class TallySerializer(serializers.ModelSerializer):
+    person = PersonSerializer(read_only=True)
+    drink = DrinkSerializer(read_only=True)
+
+    class Meta:
+        model = Tally
+        fields = ["id", "person", "drink"]
