cc25aea664769b3bcd5afae39112edc4e5001e37
LorisSchell <loris.schell@t-online.de>
2024-04-27 00:52:00 +0200

    added views

--- tally/api/views.py
+++ tally/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Person, Drink, Tally
+from .serializers import PersonSerializer, DrinkSerializer, TallySerializer
 
-# Create your views here.
+class PersonListCreate(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class DrinkListCreate(generics.ListCreateAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
+
+class TallyListCreate(generics.ListCreateAPIView):
+    queryset = Tally.objects.all()
+    serializer_class = TallySerializer
