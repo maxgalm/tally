6490af037f85fea55bbab64dfb33e47ba05625ab
LorisSchell <loris.schell@t-online.de>
2024-04-29 01:20:14 +0200

    fixed missing space in function __str__ of Person

--- tally/api/models.py
+++ tally/api/models.py
@@ -7,7 +7,7 @@ class Person(models.Model):
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + " " + self.last_name
 
 class Drink(models.Model):
     name = models.CharField(max_length=50)
