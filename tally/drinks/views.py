f9a8801b62368b65d7de1ee4e9b5f36725621ea2
LorisSchell <loris.schell@t-online.de>
2024-05-01 17:36:24 +0200

    added basic authentication to drink views

--- tally/drinks/views.py
+++ tally/drinks/views.py
@@ -1,14 +1,19 @@
+from rest_framework import generics, authentication, permissions
+
 from .models import Drink
 from .serializers import DrinkSerializer
-
-from rest_framework import generics
+from .permissions import IsDrinkEditorPermission
 
 # Create your views here.
 class DrinkListCreate(generics.ListCreateAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsDrinkEditorPermission]
 
 class DrinkRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsDrinkEditorPermission]
     lookup_field = "pk"
