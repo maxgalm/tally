4e2426047f90367a3159eaa84b4615a37cafe57a
LorisSchell <loris.schell@t-online.de>
2024-05-01 22:19:53 +0200

    added rest_framework.authtoken to the project and created an url to obtain a token

--- tally/api/urls.py
+++ tally/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 # api/
 urlpatterns = [
+    path("auth/", obtain_auth_token ),
     path("persons/", views.PersonListCreate.as_view()),
     path("persons/<int:pk>/", views.PersonRetrieveUpdateDestroy.as_view()),
     path("drinks/", include("drinks.urls")),
