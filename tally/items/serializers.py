46fc703e4ac10d978d2064c47a2a7bd17c560de1
LorisSchell <loris.schell@t-online.de>
2024-05-04 22:32:01 +0200

    modified tally item serializers and added a public user serializer

--- tally/items/serializers.py
+++ tally/items/serializers.py
@@ -1,17 +1,27 @@
+from rest_framework import serializers
+
 from .models import TallyItem
 from drinks.serializers import DrinkSerializer
-
-from rest_framework import serializers
+from api.serializers import UserPublicSerializer
 
 class TallyItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TallyItem
-        fields = ["id", "user", "drink", "created_at"]
+        fields = [
+            "id",
+            "user",
+            "drink",
+            "created_at"
+        ]
 
 class NestedTallyItemSerializer(serializers.ModelSerializer):
-    # person = PersonSerializer(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="items-detail",
+        lookup_field="pk"
+    )
+    owner = UserPublicSerializer(source="user", read_only=True)
     drink = DrinkSerializer(read_only=True)
 
     class Meta:
         model = TallyItem
-        fields = ["id", "user", "drink", "created_at"]
+        fields = ["id", "url", "owner", "drink", "created_at"]
--- tally/items/urls.py
+++ tally/items/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 # api/items/
 urlpatterns = [
-    path("", views.TallyItemListCreateAPIView.as_view()),
-    path("<int:pk>/", views.TallyItemDetailAPIView.as_view()),
-    path("<int:pk>/update/", views.TallyItemUpdateAPIView.as_view()),
-    path("<int:pk>/delete/", views.TallyItemDestroyAPIView.as_view())
+    path("", views.TallyItemListCreateAPIView.as_view(), name="items-list"),
+    path("<int:pk>/", views.TallyItemDetailAPIView.as_view(), name="items-detail"),
+    path("<int:pk>/update/", views.TallyItemUpdateAPIView.as_view(), name="items-update"),
+    path("<int:pk>/delete/", views.TallyItemDestroyAPIView.as_view(), name="items-delete")
 ]
